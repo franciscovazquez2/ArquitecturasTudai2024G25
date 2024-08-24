7650d54de70ca2ad4dad7d699615757826d83fd2
franciscovazquez2 <franciscovazquez211@gmail.com>
2024-08-24 11:22:17 -0300

    Se agrega mySQLdocker.yml / se establece main para probar conexion-versiones

--- ArquitecturasTp1Tudai2024/src/main/java/org/example/App.java
+++ ArquitecturasTp1Tudai2024/src/main/java/org/example/App.java
@@ -1,13 +1,59 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.lang.reflect.InvocationTargetException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class App {
+    public static void main(String[] args) {
+        String driver = "com.mysql.cj.jdbc.Driver";
+        try {
+            Class.forName(driver).getDeclaredConstructor().newInstance();
+        } catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException
+                 | NoSuchMethodException | SecurityException | ClassNotFoundException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        String uri = "jdbc:mysql://localhost:3306/testdb";
+        try{
+            Connection conn = DriverManager.getConnection(uri,"user", "userpassword");//en derby el parametro es solo uri
+            conn.setAutoCommit(false);//por defecto se encuentra en true y no permite commitear.
+            //createTables(conn);
+            //addPersona(conn, 1, "pancho", 33);
+            //addPersona(conn, 2, "vico", 28);
+            String select = "SELECT * FROM persona";
+            PreparedStatement ps = conn.prepareStatement(select);
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                System.out.println("id: "+rs.getInt(1)+
+                        ", name: "+rs.getString(2)+
+                        ", edad: "+rs.getInt(3));
+            }
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
+
+    public static void createTables(Connection conn) throws SQLException{
+        String table= "CREATE TABLE persona(id INT, name VARCHAR(500), edad INT, PRIMARY KEY(id))";
+        conn.prepareStatement(table).execute();
+        conn.commit();
+    }
+
+    public static void addPersona(Connection conn, int id, String name, int edad) throws SQLException{
+        String insert = "INSERT INTO persona(id, name, edad) VALUES (?,?,?)";
+        java.sql.PreparedStatement ps = conn.prepareStatement(insert);
+        ps.setInt(1, id);
+        ps.setString(2, name);
+        ps.setInt(3, edad);
+        ps.executeUpdate();
+        ps.close();
+        conn.commit();
+    }
+
 }
