817852579d624113e9b2c05c915c1d0cd900f801
Emanuel Arispe <eaperispeperez@alumnos.exa.unicen.edu.ar>
2024-08-25 18:06:16 -0300

    Se crearon los patrones DAO y FACTORY

---
+++ ArquitecturasTp1Tudai2024/src/main/java/Dao/MYSQLClientDAO.java
@@ -0,0 +1,59 @@
+package Dao;
+
+import java.sql.*;
+import java.util.List;
+
+public class MYSQLClientDAO implements CustomerDAO{
+
+    private static final String URI = "jdbc:mysql://localhost:3306/dbArquiTpG25";
+
+ // Conexcion a la base de datos
+    private Connection getConnection(){
+        try {
+            Connection conn = DriverManager.getConnection(URI, "user", "password");//en derby el parametro es solo uri
+            conn.setAutoCommit(false);
+            return conn;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    // Metodo de prueba  -- ELIMINAR
+    public void pruebaSelect() throws SQLException {
+        Connection conn = getConnection();
+        String select = "SELECT * FROM persona";
+        PreparedStatement ps = conn.prepareStatement(select);
+        ResultSet rs = ps.executeQuery();
+        while(rs.next()){
+            System.out.println("id: "+rs.getInt(1)+
+                    ", name: "+rs.getString(2)+
+                    ", edad: "+rs.getInt(3));
+        }
+        conn.close();
+    }
+
+    @Override
+    public int insertCustomer() {
+        return 0;
+    }
+
+    @Override
+    public boolean deleteCustomer() {
+        return false;
+    }
+
+    @Override
+    public List findCustomer() {
+        return List.of();
+    }
+
+    @Override
+    public boolean updateCustomer() {
+        return false;
+    }
+
+    @Override
+    public List selectCustomersRS() {
+        return List.of();
+    }
+}
--- ArquitecturasTp1Tudai2024/src/main/java/org/example/App.java
+++ ArquitecturasTp1Tudai2024/src/main/java/org/example/App.java
@@ -1,14 +1,22 @@
 package org.example;
 
-import java.lang.reflect.InvocationTargetException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import Dao.CustomerDAO;
+import Dao.MYSQLClientDAO;
+import Factory.DAOFactory;
+
 import java.sql.SQLException;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+
+
+
+
+
+    }}
+        //---------------------------------------------------------------
+
+/*
         String driver = "com.mysql.cj.jdbc.Driver";
         try {
             Class.forName(driver).getDeclaredConstructor().newInstance();
@@ -22,9 +30,9 @@ public class App {
         try{
             Connection conn = DriverManager.getConnection(uri,"user", "password");//en derby el parametro es solo uri
             conn.setAutoCommit(false);//por defecto se encuentra en true y no permite commitear.
-            //createTables(conn);
-            //addPersona(conn, 1, "pancho", 33);
-            //addPersona(conn, 2, "vico", 28);
+            createTables(conn);
+            addPersona(conn, 1, "pancho", 33);
+            addPersona(conn, 2, "vico", 28);
             //removePersona(conn,1);
             String select = "SELECT * FROM persona";
             PreparedStatement ps = conn.prepareStatement(select);
@@ -65,5 +73,5 @@ public class App {
         ps.close();
         conn.commit();
     }
-
 }
+*/
